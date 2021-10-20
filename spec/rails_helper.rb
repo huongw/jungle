dee3ada7bfd702d9238f8c005acd5ee69042a21c
huongw <whitneyhuong@gmail.com>
2021-10-20 19:54:37 +0000

    complete test for scenerio, and screenshots

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -48,6 +48,8 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = false
 
+  # Capybara.raise_server_errors = false 
+
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
---
+++ spec/support/images.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+# Helper function to open product images
+def open_asset(file_name)
+  File.open(Rails.root.join('db', 'seed_assets', file_name))
+end
